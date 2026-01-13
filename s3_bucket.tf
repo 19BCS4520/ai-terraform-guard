b4cc312eadbef011990732cd486b101fe706da0b
Parikshit Sharma <parikshitsharma@Parikshits-MacBook-Air.local>
2026-01-13 22:44:47 +0530

    Simulate Attack: Add public S3 bucket violation

---
+++ s3_bucket.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "sensitive_data" {
+  bucket = "company-financial-records-2024"
+
+  tags = {
+    Environment = "Production"
+    DataLevel   = "Confidential"
+  }
+}
+
+resource "aws_s3_bucket_acl" "sensitive_data_acl" {
+  bucket = aws_s3_bucket.sensitive_data.id
+  acl    = "public-read"
+}
